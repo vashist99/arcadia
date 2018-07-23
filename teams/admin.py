c56872261a25634fbe7f72dfc534c1eebfee8059
Neeraj <neerajrajpurohitnn@gmail.com>
2018-07-23 18:04:01 +0530

    2 new View functions added

--- teams/admin.py
+++ teams/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-from .models import user_record
+
 # Register your models here.
-admin.site.register(user_record)
--- teams/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from .views import register, checkusername
-urlpatterns = [
-    url(r'register/$', register, name = 'register'),
-    url(r'checkusername/$', checkusername, name = 'checkusername')
-]
