2cee87414a0143cb1803066b96265e617bddc096
Jay Mistry <mistry.jay15@gmail.com>
2018-07-24 11:27:41 +0530

    Improved integration - CSS and Javascript works

--- arcadia/settings.py
+++ arcadia/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'arcadia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Frontend'],
+        'DIRS': ['arcadia/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
